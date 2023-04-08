9a655877208be94a8e48bd8f9a81b50d3fda9719
Laurent <3192288+gravlor@users.noreply.github.com>
2023-04-08 03:39:23 +0200

    base project

---
+++ src/test/java/com/gravlor/josiopositioningsystem/JosioPositioningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravlor.josiopositioningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosioPositioningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
